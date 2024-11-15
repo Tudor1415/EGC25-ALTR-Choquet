ec2729da4cd798886fc5d2f0c72c4c805eab4775
Tudor Opran <tudoropran2@gmail.com>
2024-11-15 18:18:21 +0100

    Experiment works on discretized data

--- augmented_data_exp/__main__.py
+++ augmented_data_exp/__main__.py
@@ -1,31 +1,29 @@
 # main.py
 import os
 import argparse
-from maven_utils import get_maven_classpath
-from java_runner import run_extract_sample
-from data_processing import process_datasets, process_dat_files
-from model_evaluation import evaluate_datasets
 from plotting import plot_results
+from java_runner import run_extract_sample
+from maven_utils import get_maven_classpath
+from data_processing import process_datasets
+from model_evaluation import evaluate_dat_files
 
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Run ExtractSample Java program on datasets.')
-    parser.add_argument('--nb_samples', type=int, default=1_000_000, help='Number of samples to generate')
+    parser.add_argument('--nb_samples', type=int, default=100_000, help='Number of samples to generate')
     args = parser.parse_args()
 
     nb_samples = args.nb_samples
     nb_folds = 5
 
     # Paths relative to the scripts folder
-    exp_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(exp_dir, '..'))
-    dataset_folder = os.path.join(project_root, 'data', 'raw_datasets')
+    scripts_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(scripts_dir, '..'))
     dat_file_folder = os.path.join(project_root, 'data', 'dat-files') + "/"
-    mapping_file_folder = os.path.join(project_root, 'data', 'mapping-files')
     target_dir = os.path.join(project_root, 'target')
     class_output_dir = os.path.join(target_dir, 'classes')
     output_base = os.path.join(project_root, 'results', 'augmented_data_exp')
@@ -44,31 +42,31 @@ def main():
     execution_classpath = os.pathsep.join([class_output_dir, maven_classpath])
 
     # Step 2: Iterate over all dataset files in the dataset folder and run Java program
-    # for dataset_file in os.listdir(dat_file_folder):
-        # if os.path.isfile(os.path.join(dat_file_folder, dataset_file)):
-        #     dataset_name = dataset_file
-        #     # Construct the output directory
-        #     dataset_name_without_extension = os.path.splitext(dataset_name)[0]
-        #     output_directory = os.path.join(output_base, dataset_name_without_extension, 'samples')
+    for dataset_file in os.listdir(dat_file_folder):
+        if os.path.isfile(os.path.join(dat_file_folder, dataset_file)):
+            dataset_name = dataset_file
+            # Construct the output directory
+            dataset_name_without_extension = os.path.splitext(dataset_name)[0]
+            output_directory = os.path.join(output_base, dataset_name_without_extension, 'samples')
 
-        #     # Ensure the output directory exists
-        #     os.makedirs(output_directory, exist_ok=True)
+            # Ensure the output directory exists
+            os.makedirs(output_directory, exist_ok=True)
 
-        #     # Run the Java program
-        #     run_extract_sample(
-        #         dataset_name,
-        #         dat_file_folder,
-        #         output_directory,
-        #         nb_samples,
-        #         timeout_minutes,
-        #         measure_names,
-        #         weights,
-        #         execution_classpath,
-        #         project_root
-        #     )
+            # Run the Java program
+            run_extract_sample(
+                dataset_name,
+                dat_file_folder,
+                output_directory,
+                nb_samples,
+                timeout_minutes,
+                measure_names,
+                weights,
+                execution_classpath,
+                project_root
+            )
 
     # Process datasets to add rule columns
-    process_datasets(["mushroom"], dataset_folder, output_base, mapping_file_folder)
+    process_datasets(dat_file_folder, output_base)
 
     # Evaluate datasets
     models = {
@@ -76,7 +74,8 @@ def main():
         'SVM': SVC(random_state=42),
         'LogisticRegression': LogisticRegression(max_iter=1000, random_state=42)
     }
-    results = evaluate_datasets(dat_file_folder, output_base, models, nb_folds)
+    results = evaluate_dat_files(dat_file_folder, output_base, models, nb_folds)
+
 
     # Plot results
     plot_results(results, output_base)
--- augmented_data_exp/data_processing.py
+++ augmented_data_exp/data_processing.py
@@ -1,10 +1,8 @@
 # data_processing.py
 import os
 import pandas as pd
-import re
-import operator
 
-def add_rule_columns_discretized(df, rules_df):
+def add_rule_columns(df, rules_df):
     # Get the max value in the entire DataFrame
     last_item = df.max().max()
     
@@ -32,33 +30,7 @@ def add_rule_columns_discretized(df, rules_df):
     
     return df
 
-def add_rule_columns(df, rules_df, mapping):
-    # Process each of the top 10 rules
-    for index, row in rules_df.head(10).iterrows(): # Only top 10 rules
-        rule = row['Rule']
-        rule_index = index + 1
-        column_name = f"Rule_{rule_index}"
-        
-        # Parse rule to separate antecedent and consequent elements
-        antecedent, consequent = rule.split(' => ')
-        antecedent_elements = [int(x.strip()) for x in antecedent.strip('[]').split(';')]
-        consequent_elements = [int(x.strip()) for x in consequent.strip('[]').split(';')]
-        
-        # Combine all elements that need to be checked for the rule to be valid
-        all_rule_elements = antecedent_elements + consequent_elements
-        
-        # Apply rule validity check (all antecedent items are in the transaction) on each row
-        df[column_name] = df.apply(
-            lambda row: 1 
-            if all(row[mapping[elem][0]] for elem in antecedent_elements 
-                  if mapping[elem][1](mapping[elem][0], row[mapping[elem][0]])) 
-            else 0,
-            axis=1
-        )
-    
-    return df
-
-def process_dat_files(dat_file_folder, output_base):
+def process_datasets(dat_file_folder, output_base):
     # Iterate over each dataset directory
     for dataset_name in os.listdir(dat_file_folder):
         dataset_name = dataset_name.split(".")[0]
@@ -75,49 +47,13 @@ def process_dat_files(dat_file_folder, output_base):
 
                     # Add rule columns to this dataset
                     df = pd.read_csv(dat_file_folder + dataset_name + ".dat", delimiter=' ')
-                    df = add_rule_columns_discretized(df, rules_df)
+                    df = add_rule_columns(df, rules_df)
 
                     aug_directory = os.path.join(output_base, dataset_name, "aug")
 
                     os.makedirs(aug_directory, exist_ok=True)
 
-                    output_path = os.path.join(aug_directory, dataset_name + "_processed_disc.dat")
-                    print(df.head())
-                    df.to_csv(output_path, sep=' ', index=False, header=False)
-                                    
-                    print(f"Processed {dataset_name} with new rule columns.")
-                    
-                except Exception as e:
-                    print(f"Failed to process {rules_file_path}: {e}")
-
-def process_datasets(dataset_names, dataset_folder, output_base, mapping_folder):
-    # Iterate over each dataset directory
-    for dataset_name in dataset_names:
-        print()
-        mapping_file_path = os.path.join(mapping_folder, dataset_name + ".map")
-        mapping = create_conditions_dict(mapping_file_path)
-        
-        rules_path = os.path.join(output_base, dataset_name, "samples")
-        
-        # Check if the dataset directory has any files
-        if os.path.isdir(rules_path):
-            # Find the first CSV file in the samples folder
-            first_file = next((f for f in os.listdir(rules_path) if f.endswith('.csv')), None)
-            if first_file:
-                rules_file_path = os.path.join(rules_path, first_file)
-                try:
-                    # Read the rules data from the first CSV file
-                    rules_df = pd.read_csv(rules_file_path)
-
-                    # Add rule columns to this dataset
-                    df = pd.read_csv(dataset_folder + dataset_name + "csv", delimiter=';')
-                    df = add_rule_columns(df, rules_df, mapping)
-                    print(df.head())
-                    aug_directory = os.path.join(output_base, dataset_name, "aug")
-
-                    os.makedirs(aug_directory, exist_ok=True)
-
-                    output_path = os.path.join(aug_directory, dataset_name + "_processed_non_disc.dat")
+                    output_path = os.path.join(aug_directory, dataset_name + "_processed.dat")
 
                     df.to_csv(output_path, sep=' ', index=False, header=False)
                                     
@@ -125,50 +61,3 @@ def process_datasets(dataset_names, dataset_folder, output_base, mapping_folder)
                     
                 except Exception as e:
                     print(f"Failed to process {rules_file_path}: {e}")
-
-
-def create_conditions_dict(mapping_file_path):
-    map_df = pd.read_csv(mapping_file_path, sep="\t")
-    conditions_dict = {}
-    
-    # Define operator mappings
-    operators = {
-        '<': operator.lt,
-        '<=': operator.le,
-        '=': operator.eq,
-        '>=': operator.ge,
-        '>': operator.gt
-    }
-    
-    # Process each row in the DataFrame
-    for _, row in map_df.iterrows():
-        encoding = int(row['Encoding'])
-        condition_str = row['Column_Name'].strip()
-
-        # Match patterns like 'variable < value', 'variable >= value', 'variable = value', 'variable = (value1 - value2)'
-        match = re.match(r'(\w[\w\-]*)\s*(<|<=|=|>=|>)\s*(.+)', condition_str)
-        if match:
-            variable, op_str, value = match.groups()
-            op_func = operators[op_str.strip()]
-            value = value.strip().replace('(', '').replace(')', '')
-
-            # Check for range conditions (e.g., "Amount_of_given_credit_card = (45000 - 145000)")
-            if ' - ' in value and op_str == '=':
-                low, high = map(float, value.split('-'))
-                conditions_dict[encoding] = (variable.strip(), lambda var_name, var_value, var=variable, low=low, high=high: var_name == var and low <= float(var_value) <= high)
-            else:
-                # Handle simple conditions with numeric or categorical values
-                val = float(value) if value.replace('.', '', 1).isdigit() else value
-                conditions_dict[encoding] = (variable.strip(), lambda var_name, var_value, var=variable, op=op_func, val=val: var_name == var and op(float(var_value), val) if isinstance(val, (int, float)) else var_name == var and op(var_value, val))
-        else:
-            # Handle conditions without operators (e.g., "Gender = 1")
-            if '=' in condition_str:
-                variable, value = condition_str.split('=')
-                value = value.strip().replace('(', '').replace(')', '')
-                conditions_dict[encoding] = (variable.strip(), lambda var_name, var_value, var=variable.strip(), val=value: var_name == var and str(var_value) == val)
-            else:
-                # Handle direct categorical values without '=' (e.g., "class_0" and "class_1")
-                variable = condition_str.strip()
-                conditions_dict[encoding] = (variable.strip(), lambda var_name, var_value, var=variable: var_name == var and str(var_value) == '1')
-    
-    return conditions_dict
